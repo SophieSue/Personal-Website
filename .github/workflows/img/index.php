97a04f27db3143beee51181f7521b545205c07c8
Sophie Su <67069925+SophieSue@users.noreply.github.com>
2022-02-08 17:38:16 -0600

    Add files via upload

---
+++ .github/workflows/img/index.php
@@ -0,0 +1,69 @@
+
+
+<html>
+<style>
+
+
+h1 {
+	padding: 1px auto;
+	height:35px;
+`	max-width: 960px;
+	min-width:400px;
+	margin: 100px auto 1px auto;
+	width: 60%;   
+	text-align:left; 
+	font-family: Times New Roman;
+	color: black;
+	text-shadow: 1px 1px 5px #ccc9ff, 1px 1px 25px #ccc9ff, 0 0 5px white;
+	font-size: 45pt;
+	opacity: 0.6;
+	
+}
+
+body{
+	background-repeat: no-repeat;
+	background-position:center center;
+	background-attachment: fixed;
+	background-image: url(404.jpg);
+	background-color:black;
+	
+}
+
+div {
+	min-width:150px;
+	max-width: 200px;
+	width:15%;
+	height:55px;
+	margin: 40vh auto 100px auto;
+	 background: linear-gradient(to bottom, #6666ff 0%, #ffccff 100%);
+	color: white;
+	text-align: center; 
+	border-radius: 1px;
+	border: 2px dashed rgba(255,255,255, 0.8); 
+	padding: 0px auto;
+	
+}
+
+p{
+	font-size: 14pt;
+}
+
+a{
+	text-decoration: none;
+}
+
+</style>
+
+<body>
+
+<h1><i> OOPS !!</i> </h1>
+<h1> <i>404 Not Found... </i></h1>
+
+<a href="../index.php">
+<div>
+<p> <i> <b> Go Back </b></i></p>
+</div>
+</a>
+
+</body>
+</html>
